a56479af65098fea9aa1b4d1d0118214982d19eb
Anand Raj <rjanand38@gmail.com>
2026-04-19 23:54:37 +0530

    #if all possible solutions are needed #if only 1st one is need then remove results

---
+++ day7/revision/two_sum.py
@@ -0,0 +1,13 @@
+def two_sum(nums,target):
+    dict1={}
+    results = []
+    for i in range(len(nums)):
+        comp = target-nums[i]
+        if comp in dict1:
+            results.append([dict1[comp],i])
+        dict1[nums[i]]=i
+    return results
+
+#if all possible solutions are needed 
+#if only 1st one is need then remove results
+
